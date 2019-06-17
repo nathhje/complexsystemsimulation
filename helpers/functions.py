5976e9a35bd2d0460d734c57d95f2e7a35a40ba7
guyguyguyguyguyguyguy <guy.h.frankel@gmail.com>
2019-06-17 12:34:35 +0200

    start of funcitons

---
+++ helpers/functions.py
@@ -0,0 +1,17 @@
+import math
+import numpy as np
+
+def x_n1(xn, B, A, w1, w2):
+    x_n1 = B*(math.tanh(w2*xn)) - A*(math.tanh(w1*xn))
+    return x_n1
+
+def error_estim(n1, n2, xn, xp): 
+    en = (1/(n2-n1+1))*(abs((xn-xp) - xn))
+    return en
+
+def gnorm(A, An, B, Bn):
+    Adiff = ((A/An)) -1)* np.sign((A/An) - 1)
+    Bdiff = ((B/Bn)) -1)* np.sign((B/Bn) - 1)
+
+def output(An, A, B, Bn, M, w3, error_estim, z):
+    output = (1.1- ())
