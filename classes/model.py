caf4e924381a2dc8f08e78aea9edef1d2d8fa5d9
guyguyguyguyguyguyguy <guy.h.frankel@gmail.com>
2019-06-28 20:07:50 +0200

    comments

--- classes/model.py
+++ classes/model.py
@@ -23,34 +23,14 @@ class Model():
         self.In = Population(A,w1)
         self.Ex = Population(B,w2)
     
-    def bifurcation(self):
-        
-        xn = math.inf
-        Alist = []
-        xlist = []
-        
-        for A in np.arange(0.,40.,0.1):
-            
-            self.In.coef = A
-            xn1 = functions1.x_n1(xn,self.Ex,self.In)
-            print(self.In.coef)
-            Alist.append(A)
-            xlist.append(xn1)
-            
-        plt.figure()
-        plt.plot(Alist,xlist)
-        plt.show()
-    
+    #Running model
     def runModel(self):
+        #Initial parameter values and lists to store generated data (messy but works)
         A = 14.47
         B = 5.82
         An = 13.6
         Bn = 5.82
         beta = -1
-
-
-
-        #These cannot be infinity, but then what do we do?!
         x = 0.5
         x_s = [0.5 for i in range(200)]
         y_s = [0 for i in range(2)]
@@ -65,6 +45,8 @@ class Model():
         fyk_s = []
         en=0
         mu=0.002
+
+        #Iteratively running the model to produce the output for n days
         for i in range(0, 7500):
         
             counter += 1
@@ -73,7 +55,7 @@ class Model():
             if counter %100 ==0:
                 iext = 0
             
-            
+            #Function used to randomly alter the value of B every day
             # if counter %200 == 0:
             #     rand = random.randint(1,101)
             #     if rand <= 30:
@@ -81,6 +63,7 @@ class Model():
             #     else:
             #         B = 5.82
 
+            #Function used to randomly alter the value of A every two days (this seemed to work better than doing it every day)
             # if counter %400 == 0:
             #     rand = random.randint(1,101)
             #     if rand <= 37:
@@ -88,57 +71,59 @@ class Model():
             #     else:
             #         A = 14.47
 
-            # if counter %150 == 0:
-            #     mu = 0.02
-            #     rand = random.randint(1,101)
-            #     if rand <=47:
-            #         mu = mu * rand/100
-            #         print('yes')
-            #     elif 47 < rand < 95:
-            #         mu = mu * rand/15
+            #Making mu dependent on the output height
+                #This is for manic episodes
+            # if outputk_s[i-1] <= 0.01:
+            #     if outputk_s[i] >= 1.45:
+            #         mu = mu/10
             #         print('no')
             #     else:
-            #         mu = 0.02
-            # mu = 0.002
+            #         mu = 0.002
+            #     #This is for depressed episodes
             # if outputk_s[i-1] <= 0.01:
-            #     # mu = 0.002
-            #     if outputk_s[i] <=1.2:
-            #         mu = mu * 3
-            #         print('yes')
-                # else: 
-                #     mu = 0.002
-            if outputk_s[i-1] <= 0.01:
-                if outputk_s[i] >= 1.45:
-                    mu = mu/10
-                    print('no')
-                else:
-                    mu = 0.002
+            #     if 1 < outputk_s[i] <=1.3:
+            #         mu = mu/ * 2
+            #         print('no')
+            #     else:
+            #         mu = 0.002
 
-
+            #Recusively applying equation 1
             x = functions.x_n1(x, B, A, 0.2223, 1.487)
             x_s.append(x)
+
+            #Working out the error every day and keeping that error for the entirity of the day
             if counter %101 ==0:
                 en = functions.error_estim(0,2, x_s[i], x_s[i+200])
 
+            #application of equation 6
             g = functions.gdelta(0.3, A, An, B, Bn, 1, 0, 2, x_s[i], x_s[i+200])
+
+            #Equation 4
             fyk = functions.fyk(3.2, beta, iext, y_s[i+1], y_s[i])
             fyk_s.append(fyk)
 
+            #Equation 3
             ykp1, zk = functions.Rulkov(3.2, -2.5780, iext, y_s[i+1], y_s[i], zk_s[i], -0.8, 1.3, mu, g)
             zk_s.append(zk)
             y_s.append(ykp1)
 
-
+            #Equation 5, the output
             outputk = functions.output(An, A, B, Bn, 1, en, zk)
 
-            if B == 7.65:
-                if outputk >1.5:
-                    outputk = outputk *1.15
-                    # print(outputk)
+            #Artistic licence, to better represent the differences in episodes and to better match the paper
+            # if B == 7.65:
+            #     if outputk >1.5:
+            #         outputk = outputk *1.15
+
+            # if A == 16.47:
+                #if 1 < outputk < 1.35:
+                    #outputk = outputk * 0.8
 
             outputk_s.append(outputk)
             n.append(i)
 
+        #PLotting the output per day
+        #Making x axis days
         n = [x / 250 for x in n]
 
         plt.plot(n, outputk_s[1:], c='blue')
@@ -169,7 +154,7 @@ class Model():
         # plt.plot(i_s[1:], fykp)
         # plt.show()
 
-        # #Plot 2
+        # PLotting the Rulkov map output for our model
         Iext = 0
         i_s=[]
         i_s.append(-0.21)
