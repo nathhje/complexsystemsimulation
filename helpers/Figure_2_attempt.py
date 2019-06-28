996ea8fd1ed5305ff29d358846b63b1ccec5346b
guyguyguyguyguyguyguy <guy.h.frankel@gmail.com>
2019-06-28 19:57:01 +0200

    comments

--- helpers/Figure_2_attempt.py
+++ helpers/Figure_2_attempt.py
@@ -2,61 +2,31 @@ import math
 import numpy as np 
 import matplotlib.pyplot as plt
 
+#Equation 1
 def x_n1(xn, B, A, w1, w2):
     x_n1 = B*(np.tanh(w2*xn)) - A*(np.tanh(w1*xn))
     return x_n1
 
+#Equation 2
 def error_estim(n1, n2, xn, xp): 
     en = (1/(n2-n1+1))*(abs((xn-xp) - xn))
     return en
 
-x = math.inf
-x2 = -(math.inf)
+#PLotting x as a function of iterations, this was done to produce the bifuration map and produce plots that show the dynamics of x at each value of A/B
+x = 0.5
+x2 = -0.5
 x_set = []
+x1_set = []
 x2_set = []
 B_set = []
-for A in np.arange(0, 40, 0.1):
-    for i in range(0, 400):
-        y = x_n1(x, 5.82, A, 0.2223, 1.487)
-        y2 = x_n1(x2, 5.82, A, 0.2223, 1.487)
-        # x = error_estim(1,50, x_set[period], x1)
-        # Plot the two lines.
-        # plt.plot([x, x], [x, y], 'k', lw=1)
-        # plt.plot([x, y], [y, y], 'k', lw=1)
-
-        # Plot the positions with increasing
-        # opacity.
-        # plt.plot([x], [y], 'ok', ms=10, alpha=(i + 1) / n)
-        x = y
-        x2=y2
-    for j in range(0, 100):
-        y = x_n1(x, 5.82, A, 0.2223, 1.487)
-        y2 = x_n1(x2, 5.82, A, 0.2223, 1.487)
-        x = y
-        x2 = y2
-        x_set.append(x)
-        x2_set.append(x2)
-        B_set.append(A)
-
-    # plt.show()
-
-
-
-plt.scatter(B_set, x_set, s=0.03)
-plt.scatter(B_set, x2_set, s=0.03)
-plt.show()
-
-
-
-x = math.inf
-x2 = -(math.inf)
-x_set = []
-x2_set = []
-B_set = []
+color = []
 for B in np.arange(0, 20, 0.1):
-    for i in range(0, 400):
-        y = x_n1(x, B, 14.52, 0.2223, 1.487)
-        y2 = x_n1(x2, B, 14.52, 0.2223, 1.487)
+    x_color = []
+    xas = []
+    ias = []
+    for i in range(0, 10):
+        y = x_n1(x, B, 14.47, 0.2223, 1.487)
+        y2 = x_n1(x2, B, 14.47, 0.2223, 1.487)
         # Plot the two lines.
         # plt.plot([x, x], [x, y], 'k', lw=1)
         # plt.plot([x, y], [y, y], 'k', lw=1)
@@ -65,20 +35,48 @@ for B in np.arange(0, 20, 0.1):
         # opacity.
         # plt.plot([x], [y], 'ok', ms=10, alpha=(i + 1) / n)
         x = y
+        x_set.append(x)
         x2=y2
     for j in range(0, 100):
-        y = x_n1(x, B, 14.52, 0.2223, 1.487)
-        y2 = x_n1(x2, B, 14.52, 0.2223, 1.487)
+        y = x_n1(x, B, 14.47, 0.2223, 1.487)
+        y2 = x_n1(x2, B, 14.47, 0.2223, 1.487)
         x = y
         x2 = y2
-        x_set.append(x)
+        x1_set.append(x)
         x2_set.append(x2)
         B_set.append(B)
-
+        x_color.append(x)
+        xas.append(x)
+        ias.append(j)
+    # plt.scatter(ias, xas, c='r',s=8.9, zorder=2)
+    # plt.plot(ias, xas, zorder=1)
+    # plt.title('Values x takes as a function of iteration steps with A = %s' %A)
+    # plt.ylabel('x')
+    # plt.xlabel('k')
     # plt.show()
+    color.append(list(set(x_color)))
+
+#Colouring the bifurcation diagram based on density of values x takes at eeach value of A/B
+z = []
+for aasd in color:
+    if len(aasd) < 3:
+        for i in range(100):
+            z.append(1)
+    elif 3<= len(aasd) < 25:
+        for i in range(100):
+            z.append(2)
+    elif 25<= len(aasd) < 101:
+        for i in range(100):
+            z.append(3)
 
 
-
-plt.scatter(B_set, x_set, s=0.01)
-plt.scatter(B_set, x2_set, s=0.01)
+#Plotting the bifurcation map
+plt.rcParams['axes.facecolor'] = 'grey'
+plt.scatter(B_set, x1_set, s=0.03, c='b', cmap='Reds')
+plt.scatter(B_set, x2_set, s=0.03, c='r', cmap='Reds')
+plt.title('Bifurcation map of values of x with changing B')
+plt.ylabel('$x_{inf}$')
+plt.xlabel('B')
 plt.show()
+
+
--- helpers/functions_guy.py
+++ helpers/functions_guy.py
@@ -1,24 +1,33 @@
 import math
 import numpy as np
 
+#Equation numbers given corresponding to the paper
+
+#Equation 1- recusively gives value of x at the next time step
 def x_n1(xn, B, A, w1, w2):
     x_n1 = B*(math.tanh(w2*xn)) - A*(math.tanh(w1*xn))
     return x_n1
 
+#Equation 2 - Gives the difference between x between the current x and the value of x period p, previous
+#We set the period to be 1 day
+#n represents the interval in which this period resides
 def error_estim(n1, n2, xn, xp): 
     en = (1/(n2-n1+1)) * abs(xp - xn)
     return en
 
+#Accesory equation, used in equation 4
 def gnorm(A, An, B, Bn):
     Adiff = ((A/An) -1)* np.sign((A/An) - 1)
     Bdiff = ((B/Bn) -1)* np.sign((B/Bn) - 1)
     gnorm = Adiff - Bdiff
     return gnorm
 
+#Equation 5- Gives the output, zk is scaled by the error and gnorm
 def output(An, A, B, Bn, w3, en, zk):
     output = (1.1- (gnorm(A, An, B, Bn)) * np.tanh(w3 * en))*zk
     return output
 
+#Equation 6- Used in the Rulkov map (Equation 3) to integrade the error and differences in neurotransmitter levels in the pulse generation
 def gdelta(gopt, A, An, B, Bn, wd, n1,n2,xn,xp):
     delta = np.tanh(wd* error_estim(n1,n2,xn,xp))
     Adiff = ((A/An) -1)* np.sign((A/An) - 1) * delta 
@@ -26,6 +35,7 @@ def gdelta(gopt, A, An, B, Bn, wd, n1,n2,xn,xp):
     g = gopt + (Adiff - Bdiff)
     return g
 
+#Equation 4- Used in working out the value of y in the Rulkov map (Equation 3)
 def fyk(alpha, beta, Iext, yk, ykm1):
     u = beta + Iext
     # print(0 < yk < (alpha + u))
@@ -38,9 +48,10 @@ def fyk(alpha, beta, Iext, yk, ykm1):
     elif (yk >= (alpha + u)) or (ykm1 > 0):
         return -1
 
-
+#Equation 3- The generation of the Rulkov map, which we use to generate circadian activity, as it is a pulse generator
+#Was origionally designed to produce neurone action potentials, but also does well mimicing circadian activity (after some modificaitons)
 def Rulkov(alpha, beta, Iext, yk, ykm1, zk, yp, zs, mu, g):
-    yth = 0.5
+    yth = 0.01
     
     def epsilon(y):
         return np.heaviside((y - yth),0)
